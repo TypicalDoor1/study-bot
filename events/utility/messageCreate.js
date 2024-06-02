827076e447c17298a50c58046a21acac467b31f8
TypicalDoor1 <123847012+TypicalDoor1@users.noreply.github.com>
2024-06-02 20:58:31 +0000

    fixes

--- events/utility/messageCreate.js
+++
@@ -1,4 +0,0 @@
-client.on("messageCreate", function(message){
-    console.log(`a message was created`);
-    console.log({message});
-});
--- index.js
+++ index.js
@@ -12,5 +12,17 @@ client.once(Events.ClientReady, readyClient => {
     console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
 
 client.login(process.env.DISCORD_TOKEN);
