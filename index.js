c0ada8bcdf1f33325e6127514bbbe8ee05dec5b9
TypicalDoor1 <123847012+TypicalDoor1@users.noreply.github.com>
2024-06-02 20:38:48 +0000

    fix

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
 
-client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
 const dotenv = require('dotenv');
